a28ab23956e9923bdcddc432cac8199300920854
prashansa joshi <joshirashansa11.gmail.com>
2024-09-18 23:07:46 +0545

    added the ebs permission

--- terraform/data.tf
+++ terraform/data.tf
@@ -34,7 +34,7 @@ data "aws_subnet" "subnet" {
   id       = each.value
 }
 
-data "aws_iam_policy_document" "s3_bucket_policy" {
+data "aws_iam_policy_document" "minimal_s3_permissions" {
 
   statement {
     sid = "AllowS3OperationsOnElasticBeanstalkBuckets"
