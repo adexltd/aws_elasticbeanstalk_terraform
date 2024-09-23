02f053918cceb5c4f46a2b4af65726d1d6b76b42
prashansa joshi <joshirashansa11.gmail.com>
2024-09-23 16:23:49 +0545

    nginx restart

--- my_application/connection.js
+++ my_application/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,       // Replace with your MySQL host
-  user: process.env.DB_USERNAME,            // Replace with your MySQL username
+  user: process.env.DB_USER,            // Replace with your MySQL username
   password: process.env.DB_PASSWORD,            // Replace with your MySQL password
   database: process.env.DB_NAME,     // Replace with your MySQL database name
   port: process.env.DB_PORT
