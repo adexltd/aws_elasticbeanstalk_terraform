fc2e29c1ee64b6d62f136630f26451f59794f5f0
prashansa joshi <joshirashansa11.gmail.com>
2024-09-23 17:48:39 +0545

    security group added

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -77,24 +77,25 @@ resource "aws_security_group" "eb_instances" {
   vpc_id      = data.aws_vpc.adex_poc_default_vpc.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"] # Allow HTTP traffic
     security_groups = [aws_security_group.alb.id]
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"] # Allow HTTPS traffic
+    security_groups = [aws_security_group.alb.id]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
 }
