06fddd4c0ad8356d26f7f58aa5b20dd02dc746ad
prashansa joshi <joshirashansa11.gmail.com>
2024-09-26 01:57:19 +0545

    updated the security group

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -24,20 +24,20 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # egress {
-  #   from_port       = 8080
-  #   to_port         = 8080
-  #   protocol        = "tcp"
-  #   security_groups = [aws_security_group.eb_instances.id] # Allow traffic to EB instances on port 8080
-  # }
+  egress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eb_instances.id] # Allow traffic to EB instances on port 8080
+  }
 
   # Outbound rule to allow all traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
   tags = module.naming.resources.alb.tags
 }
 
