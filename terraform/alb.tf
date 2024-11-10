8bdd68cf5445016b6fd2e2b5d0ffdf8d5a4a7808
prashansa joshi <joshirashansa11.gmail.com>
2024-11-10 21:52:12 +0545

    timeout of alb is incresed

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -30,7 +30,7 @@ module "alb" {
 
       health_check = {
         enabled             = true
-        path                = "/health"
+        path                = "/"
         protocol            = "HTTP"         # Customize this based on your needs
         matcher             = "200-402"      # HTTP status codes that indicate a healthy response
         interval            = 50             # Time in seconds between health checks
