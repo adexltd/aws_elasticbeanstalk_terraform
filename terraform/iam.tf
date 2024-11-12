67303830e1d67e1dee6a1fdf4d7d11e2b5f8fd11
prashansa joshi <joshirashansa11.gmail.com>
2024-11-12 14:25:38 +0545

    added the iam role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,29 +19,6 @@
 #   policy_arn = aws_iam_policy.secrets_manager_policy.arn
 # }
 
-# Create the IAM role
-resource "aws_iam_role" "my_iam_role" {
-  name = "my-iam-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com" # Or ECS, Lambda, etc.
-        }
-      }
-    ]
-  })
-}
-
-# Attach the policy to the dynamically created IAM role
-resource "aws_iam_role_policy_attachment" "attach_secrets_manager_policy" {
-  role       = aws_iam_role.my_iam_role.name
-  policy_arn = aws_iam_policy.secrets_manager_policy.arn
-}
 
 # resource "aws_iam_role" "elastic_beanstalk_service_role" {
 #   name = "aws-elasticbeanstalk-service-role"
