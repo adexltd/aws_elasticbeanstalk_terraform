7cfc8ac2486b0cffde28af70fc8adf5a7e14920f
prashansa joshi <joshirashansa11.gmail.com>
2024-11-12 13:15:45 +0545

    applying the terrfaorm

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -46,7 +46,7 @@ locals {
 
   ### secrets_manager
   secrets_manager = {
-    name_prefix           = "${var.naming_environment}-eb-rds-secretss"
+    name_prefix           = "${var.naming_environment}-eb-rdss-secretss"
     description           = "Secrets Manager secret for RDS database"
     region                = var.region
     enable_rotation       = false
