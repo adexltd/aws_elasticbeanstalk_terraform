9185d2f2718d094fe743e932e6b0b85623064d2f
prashansa joshi <joshirashansa11.gmail.com>
2024-11-14 09:55:25 +0545

    configuring shared alb

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -55,7 +55,7 @@ module "alb" {
   version = "1.10.0"
 
   vpc_id              = module.vpc.vpc_id
-  subnet_ids          = module.vpc.public_subnet
+  subnet_ids          = module.vpc.public_subnets
   access_logs_enabled = false
 
   attributes = ["shared"]
