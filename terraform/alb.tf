f5e3435a49071cf47330349360dfc1d29fed655d
prashansa joshi <joshirashansa11.gmail.com>
2024-11-14 08:10:37 +0545

    destroying infra

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -48,3 +48,15 @@ module "alb" {
   tags = module.naming.resources.alb.tags
 
 }
+
+
+# module "alb" {
+#   source  = "cloudposse/alb/aws"
+#   version = "1.10.0"
+
+#   vpc_id              = module.vpc.vpc_id
+#   subnet_ids          = module.subnets.public_subnet_ids
+#   access_logs_enabled = false
+
+#   attributes = ["shared"]
+# }
