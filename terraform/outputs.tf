a86f3dcd2ac39f7d0bcf3de39d5f0d559b38d25d
prashansa joshi <joshirashansa11.gmail.com>
2024-11-14 15:24:52 +0545

    applying the terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,9 @@ output "latest_ubuntu_ami_id" {
   value = data.aws_ami.ami_id.id
 }
 
-output "alb_arn" {
-  value = module.alb.alb_arn
-}
+# output "alb_arn" {
+#   value = module.alb.alb_arn
+# }
 
 output "artifact_bucket" {
   value = module.s3_bucket.s3_bucket_id
